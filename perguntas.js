c33c93c08c93b463f888f2d892eef8a07bcdf3d1
annelizezinco <142610752+annelizezinco@users.noreply.github.com>
2024-09-13 08:49:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'Biologia',
+    'Qual é a importância da fotossíntese?',
+    'A fotossíntese é o processo pela qual as plantas, algas e algumas bactérias transformam a energia solar em energia química, produzindo oxigênio e glicose.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a diferença entre célula procarionte e eucarionte?',
+    'Células procariontes não possuem núcleo definido e organelas membranosas, enquanto células eucariontes possuem um núcleo delimitado por membrana e diversas organelas membranosas.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é genética?',
+    'Genética é o estudo dos genes, que são unidades de informação hereditária presentes no DNA. Ela investiga como os genes são transmitidos de geração em geração e como influenciam as características dos organismos.'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz mar em inglês?',
+    'Sea'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de -4?',
+    'É mais ou menos 2i'
+)
+
+criaCartao(
+    'Química',
+    'O que significa densidade?',
+    'É quanto determinada massa de um elemento ocupa certo espaço'
+)
+
+criaCartao(
+    'Física',
+    'Por qual descoberta Albert Einstein ganhou o prêmio nobel de física?',
+    'Efeito fotoelétrico'
+)
+
