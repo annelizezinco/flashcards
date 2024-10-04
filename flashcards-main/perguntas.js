e239e7cdaa5d71f143dc5f49e8497a96c1f5e363
annelizezinco <142610752+annelizezinco@users.noreply.github.com>
2024-10-04 09:02:27 -0300

    Add files via upload

---
+++ flashcards-main/perguntas.js
@@ -0,0 +1,66 @@
+criaCartao(
+    'Biologia',
+    'Qual é a importância da fotossíntese?',
+    'A fotossíntese é o processo pela qual as plantas e bactérias transformam a energia solar em energia química.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é um meridiano?',
+    'Linha imaginária que vai do polo norte ao polo sul.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a diferença entre célula procarionte e eucarionte?',
+    'Células procariontes não possuem núcleo definido, enquanto células eucariontes possuem um núcleo delimitado por membrana.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são recursos naturais?',
+    'Materiais fornecidos pela natureza, como água, minerais e florestas.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são genes?',
+    'São unidades de informação hereditária presentes no DNA.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'Rio Nilo.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é um autótrofo?',
+    'Organismo que produz seu próprio alimento.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é um fuso horário?',
+    'Região da Terra com a mesma hora padrão.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 25% de 300?',
+    '75.'
+)
+
+criaCartao(
+    'Química',
+    'O que significa densidade?',
+    'É quanto determinada massa de um elemento ocupa certo espaço'
+)
+
+criaCartao(
+    'Física',
+    'Por qual descoberta Albert Einstein ganhou o prêmio nobel de física?',
+    'Efeito fotoelétrico'
+)
+
